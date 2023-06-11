64c57850e9f3c787aa2d3c2d624b5259f25fb7b8
RuijuanQi\ruijuan <hanyixuanten@outlook.com>
2023-06-11 14:13:15 +0800

    优化界面

--- index.php
+++ index.php
@@ -6,15 +6,17 @@
         <script>
             function fun(){}
             function a(){}
+            const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
+
         </script>
     </head>
     <body>
-        <div style="display:flex; justify-content:center; align-items:center;">
+        <div style="display:flex; justify-content:center; align-items:center;height:100vh;" id="m">
             <div style="display:flex; flex-direction:column; justify-content:center; align-items:center;">
                 <label for="q" style="margin-bottom:10px;">搜索:</label>
                 <input type="text" name="q" id="q" style="padding:5px; border-radius:5px; border:none; box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.75);"></input>
                 <button onclick="fun()" style="margin-top:10px; padding:5px 10px; border-radius:5px; border:none; box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.75); background-color:#4CAF50; color:white;">Submit</button>
-                <p id="gen" ></p>
+                <p id="gen" style="font-size: 20px;color: blue;"></p>
             </div>
         </div>
         <iframe id="show" title="show" height="100%" width="100%" scrolling="auto" frameborder="0" onload="a()">
@@ -33,6 +35,7 @@
             function a(){
                 gen.innerHTML="搜索完毕";
                 show.height = document.documentElement.clientHeight-200;
+                document.getElementById("m").style="display:flex; justify-content:center; align-items:center;";
             }
         </script>
     </body>
