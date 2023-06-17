c466e0fafc8c4da43b9d05b576064e11c71776d1
RuijuanQi\ruijuan <hanyixuanten@outlook.com>
2023-06-17 14:18:07 +0800

    更新界面有一键下载

--- update/update.php
+++ update/update.php
@@ -7,12 +7,14 @@
     curl_setopt($ch, CURLOPT_HTTPHEADER, ['User-Agent: PHP']);
     $response = json_decode(curl_exec($ch), true);
     curl_close($ch);
-    $latest_v=$response['tag_name'];
+    $latest_js=$response;
+    $latest_v=$latest_js['tag_name'];
     $now_v=json_decode(file_get_contents("manifast.json"),true)["version"];
-    if($latest_v==$now_v){
+    if($latest_js['tag_name']==$now_v){
         echo 'This is the latest relese!';
     }else{
         echo 'There is a new relese!';
-        echo "<a href='https://github.com/hanyixuanten/photo_searcher/releases/latest' style='background-color: #4CAF50; color: white; padding: 12px 20px; text-align: center; text-decoration: none; display: inline-block; border-radius: 4px; font-size: 16px;' target=\"_blank\">Download</a>";
+        echo "<a href='https://api.github.com/repos/hanyixuanten/photo_searcher/zipball/$latest_v' style='background-color: #4CAF50; color: white; padding: 12px 20px; text-align: center; text-decoration: none; display: inline-block; border-radius: 4px; font-size: 16px;' target=\"_blank\">Download zip</a>";
+        echo "<a href='https://api.github.com/repos/hanyixuanten/photo_searcher/tarball/$latest_v' style='background-color: #4CAF50; color: white; padding: 12px 20px; text-align: center; text-decoration: none; display: inline-block; border-radius: 4px; font-size: 16px;' target=\"_blank\">Download tar.gz</a>";
     }
 ?>
