20a5d71efd318d5f450a5c2970e091c1233959f0
RuijuanQi\ruijuan <hanyixuanten@outlook.com>
2023-06-24 16:40:53 +0800

    添加index.css，优化整体美观度

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="zh-CN">
+    <link rel="stylesheet" href="./index.css">
     <head>
         <meta charset="UTF-8"/>
         <title>图片搜索</title>
@@ -9,11 +10,11 @@
         </script>
     </head>
     <body>
-        <a href='update/update.php' style='background-color: #4CAF50; color: white; padding: 12px 20px; text-align: center; text-decoration: none; display: inline-block; border-radius: 4px; font-size: 16px;'>查找更新</a>
-        <div style="display:flex; justify-content:center; align-items:center;height:100vh;" id="m">
-            <div style="display:flex; flex-direction:column; justify-content:center; align-items:center;">
+        <a href='update/update.php' id="upd_button">查找更新</a>
+        <div id="div1" style="display:flex; justify-content:center; align-items:center;height:100vh;">
+            <div id="div2">
                 <label for="q" style="margin-bottom:10px;">搜索:</label>
-                <input type="text" name="q" id="q" style="padding:5px; border-radius:5px; border:none; box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.75);"></input>
+                <input type="text" id="q" style="padding:5px; border-radius:5px; border:none; box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.75);"></input>
                 <button onclick="fun()" style="margin-top:10px; padding:5px 10px; border-radius:5px; border:none; box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.75); background-color:#4CAF50; color:white;">Submit</button>
                 <p id="gen" style="font-size: 20px;color: blue;"></p>
             </div>
@@ -26,15 +27,15 @@
             function fun(){
                 gen.innerHTML="正在搜索";
                 show.src="search.php?q="+document.getElementById("q").value;
-                show.height = document.documentElement.clientHeight-250;
+                show.height = document.documentElement.clientHeight-260;
             }
             window.onresize = function() {
-                show.height = document.documentElement.clientHeight-250;
+                show.height = document.documentElement.clientHeight-260;
             }
             function a(){
                 gen.innerHTML="搜索完毕";
-                show.height = document.documentElement.clientHeight-250;
-                document.getElementById("m").style="display:flex; justify-content:center; align-items:center;";
+                show.height = document.documentElement.clientHeight-260;
+                document.getElementById("div1").style="display:flex; justify-content:center; align-items:center;";
             }
         </script>
     </body>
