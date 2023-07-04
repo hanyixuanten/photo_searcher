5184926f6a0677715b6eb0f61794e2f48193766b
RuijuanQi\ruijuan <hanyixuanten@outlook.com>
2023-07-04 21:21:22 +0800

    更改缩进

--- search.php
+++ search.php
@@ -1,39 +1,23 @@
-<!DOCTYPE html>
-<html lang="ZH-CN">
-	<head>
-		<meta charset="utf-8">
-		<title>搜索</title>
-	</head>
-	<?php
-		$api_response=[];
-		$p=$_GET [ 'q' ];
-		if($p!=""){
-			$api_query = "{\"fn_index\":0,\"data\":[\"$p\",1000,\"中文CLIP(Base)\",\"是\"]}";
-			$ch = curl_init();
-			curl_setopt($ch, CURLOPT_URL, 'https://modelscope.cn/api/v1/studio/damo/chinese_clip_applications/gradio/api/predict/');
-			curl_setopt($ch, CURLOPT_POST, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $api_query);
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch,CURLOPT_TIMEOUT, 120);
-			$rp=curl_exec($ch);
-			$api_response = json_decode($rp, true)["data"][0];
-			$httpcode=curl_getinfo($ch,CURLINFO_HTTP_CODE);
-			if($httpcode==500){
-				echo "您输入的内容不符合网站要求，请您更换后试试~";
-			}
-			elseif($httpcode!=200){
-				echo "error";
-			}
-			curl_close($ch);
-		}	
-	?>
-	<body>
-		<br>		 
-		<script>
-			var api_response = <?=json_encode($api_response)?>;
-			for (var i = 0; i < api_response.length; i++) {
-				document.write("<img src='" + api_response[i] + "' alt='" + api_response[i] + "'>");
-			}
-		</script>
-	</body>
-</html>
+<?php
+	$api_response=[];
+	$p=$_GET [ 'q' ];
+	if($p!=""){
+		$api_query = "{\"fn_index\":0,\"data\":[\"$p\",1000,\"中文CLIP(Base)\",\"是\"]}";
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, 'https://modelscope.cn/api/v1/studio/damo/chinese_clip_applications/gradio/api/predict/');
+		curl_setopt($ch, CURLOPT_POST, true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $api_query);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch,CURLOPT_TIMEOUT, 120);
+		$rp=curl_exec($ch);
+		$api_response = json_decode($rp, true)["data"][0];
+		$httpcode=curl_getinfo($ch,CURLINFO_HTTP_CODE);
+		if($httpcode==500){
+			echo "您输入的内容不符合网站要求，请您更换后试试~";
+		}
+		elseif($httpcode!=200){
+			echo "error";
+		}
+		curl_close($ch);
+	}	
+?>
