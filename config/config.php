70449ef998e4625ba446c1f8c2c45f5c22779d84
RuijuanQi\ruijuan <hanyixuanten@outlook.com>
2024-07-12 16:45:49 +0800

    更新配置文件，优化代码格式，提升用户体验

--- config/config.php
+++ config/config.php
@@ -1,4 +1,4 @@
 <?php
-    $use_hitokoto=true; //是否使用一言
-    // $use_update= true; //是否使用update
+$use_hitokoto = true; //是否使用一言
+$use_update = true; //是否使用update
 ?>
--- index.php
+++ index.php
@@ -1,24 +1,27 @@
 <!DOCTYPE html>
 <?php
-    include "config/config.php";
+include "config/config.php";
 ?>
 <html lang="zh-CN">
 <link rel="stylesheet" href="./index.css">
+
 <head>
     <meta charset="UTF-8" />
     <title>图片搜索</title>
 </head>
 
 <body>
-    <a href='update/update.php' id="upd_button">查找更新(for the admin)</a>
-        <?php 
-        if($use_hitokoto){
-            echo "<div id=\"yiyan\"><!-- 一言 -->\n";
-            echo "<p>一言</p>\n";
-            echo "<hr />\n";
-            echo json_decode(file_get_contents("https://international.v1.hitokoto.cn?c=d&e&f&g&h&i&j&k&l"),true)["hitokoto"];
-        }
-        ?>
+    <?php
+    if ($use_update) {
+        echo "<a href='update/update.php' id=\"upd_button\">查找更新(for the admin)</a>\n";
+    }
+    if ($use_hitokoto) {
+        echo "<div id=\"yiyan\"><!-- 一言 -->\n";
+        echo "<p>一言</p>\n";
+        echo "<hr />\n";
+        echo json_decode(file_get_contents("https://international.v1.hitokoto.cn?c=d&e&f&g&h&i&j&k&l"), true)["hitokoto"];
+    }
+    ?>
     </div>
     <div id="select"><!-- 搜索类型选择 -->
         <label for="searchType">搜索类型:</label>
@@ -30,17 +33,13 @@
     <div id="div1" style="display:flex; justify-content:center; align-items:center;height:100vh;"><!-- 搜索框 -->
         <div id="div2">
             <label for="inputs" style="margin-bottom:10px;">搜索内容:</label>
-            <input name="inputs" type="text" id="inputs"
-                style="padding:5px; border-radius:5px; border:none; box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.75);"></input>
+            <input name="inputs" type="text" id="inputs" style="padding:5px; border-radius:5px; border:none; box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.75);"></input>
             <form id="form1" target="show" method="post" action="search.php">
                 <input type="text" id="base64" name="inputs" style="display: none;"></input>
-                <button id="fun" type="submit"
-                    style="margin-top:10px; padding:5px 10px; border-radius:5px; border:none; box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.75); background-color:#4CAF50; color:white;">Submit</button>
+                <button id="fun" type="submit" style="margin-top:10px; padding:5px 10px; border-radius:5px; border:none; box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.75); background-color:#4CAF50; color:white;">Submit</button>
                 <br><br>
                 <label for="num" style="margin-bottom:10px;">搜索数量:</label>
-                <input name="num" type="number" id="num"
-                    style="padding:5px; border-radius:5px; border:none; box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.75);"
-                    min="10" max="1000" value="50"></input>
+                <input name="num" type="number" id="num" style="padding:5px; border-radius:5px; border:none; box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.75);" min="10" max="1000" value="50"></input>
                 <br>
                 <p id="gen" style="font-size: 20px;color: blue;"></p>
             </form>
@@ -49,4 +48,5 @@
     <iframe name="show" id="show" title="show" height="100%" width="100%" scrolling="auto" frameborder="0"></iframe>
     <script src="script.js"></script>
 </body>
+
 </html>
